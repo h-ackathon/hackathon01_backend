9c2d38296546b856079eb927e46483b151b649a0
Usman Ali Khan <usmanalikhan00@bitbucket.com>
2019-11-30 02:30:30 +0500

    added the teams api folder

--- models/scorecard.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const {Schema} = mongoose;
-
-const ScorecardSchema = new Schema({
-    match: {type: Schema.Types.ObjectId, ref: 'matches'},
-    players: [{type: Schema.Types.ObjectId, ref: 'players'}]
-});
-
-module.exports = mongoose.model('scorecards', ScorecardSchema);
--- routes.js
+++ routes.js
@@ -1,9 +1,9 @@
 var AuthComponent = component('Auth');
 var LatestNewsComponent = component('LatestNews');
-var TeamRankingsComponent = component('TeamRankings');
+var TeamsComponent = component('Teams');
 
 module.exports = function(router){
     AuthComponent(router);
     LatestNewsComponent(router);
-    TeamRankingsComponent(router);
+    TeamsComponent(router);
 }
