e525ce36237b420a952387605a4115466a67b075
Bilal Asif <bilal.asif@khaleef.com>
2019-11-30 00:36:20 +0500

    teams seeding

---
+++ seeding-team.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const { Seeder } = require('mongo-seeding');
+process.env.DEBUG = 'mongo-seeding';
+const config = {
+    database: 'mongodb://localhost:27017/cricket',
+    dropDatabase: false,
+};
+const seeder = new Seeder(config);
+const collections = seeder.readCollectionsFromPath(path.resolve("./team-seeding"),
+{
+  transformers: [Seeder.Transformers.replaceDocumentIdWithUnderscoreId],
+});
+console.log("Collections-->>", collections);
+seeder.import(collections)
+.then(function(){
+    console.log("Seeder success");
+})
+.catch(function(err){
+    console.log(err);
+});
