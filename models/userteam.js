58a99a193f7a155c8dad796344f7b2f9ddb111bf
Bilal Asif <bilal.asif@khaleef.com>
2019-11-30 06:37:17 +0500

    stat seeding

--- models/userteam.js
+++ models/userteam.js
@@ -1,10 +1,8 @@
 const mongoose = require('mongoose');
 const {Schema} = mongoose;
-
 const UserTeamSchema = new Schema({
     match: {type: Schema.Types.ObjectId, ref: 'matches'},
     players: [{type: Schema.Types.ObjectId, ref: 'players'}],
     user: {type: Schema.Types.ObjectId, ref: 'users'}
 });
-
 module.exports = mongoose.model('userteams', UserTeamSchema);
