a698cf46a2a24342f11670d6efd8361ea015baa2
Bilal Asif <bilal.asif@khaleef.com>
2019-11-30 06:43:03 +0500

    unnecessary files removed

--- models/userteam.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const {Schema} = mongoose;
-const UserTeamSchema = new Schema({
-    league: {type: Schema.Types.ObjectId, ref: 'leagues'},
-    players: [{isCaptain: Boolean, isViceCaptain: Boolean, id: { type: Schema.Types.ObjectId, ref: 'players' }}],
-    user: {type: Schema.Types.ObjectId, ref: 'users'},
-    name: {type: String}
-});
-module.exports = mongoose.model('userteams', UserTeamSchema);
