24c46c1a1d04892832c2f9b62378b0f2403c14f6
Bilal Asif <bilal.asif@cricingif.com>
2020-01-14 12:50:54 +0500

    added teams data, homepage, starting work on prediction forms

--- frontend/home/home.ctrl.js
+++ frontend/home/home.ctrl.js
@@ -1,20 +1,24 @@
-const { dateFormat, checkEquality, getFirstCharIfNoImage } = require('../../helpers/app_helper');
+const { dateFormat, checkEquality, getFirstCharIfNoImage, getMatchTitle } = require('../../helpers/app_helper');
 const { checkTokenValidity } = require('../../helpers/api_helper');
 const {getAllLeagues} = require('../../helpers/league_helper');
+const t20 = require('../../matches_data/t20i');
 
 exports.index = async (req, res) => {
 
-    const d = await getAllLeagues();
+    // const d = await getAllLeagues();
+
+    console.log("t20is", t20)
     
     let dataHash = {
-        data: d,
+        data: t20,
         name: req.cookies.name,
         isLoggedIn: req.cookies.token && req.cookies.token !== 'undefined' 
         && checkTokenValidity(req.cookies.token),
         helpers: {
             dateFormat: dateFormat,
             checkEquality: checkEquality,
-            getFirstCharIfNoImage: getFirstCharIfNoImage
+            getFirstCharIfNoImage: getFirstCharIfNoImage,
+            getMatchTitle: getMatchTitle
         }
     };
     res.render('home', dataHash);
--- helpers/app_helper.js
+++ helpers/app_helper.js
@@ -5,3 +5,28 @@ exports.dateFormat = (date) => timeago().format(date);
 // exports.getJobDetails = (job, key) => JSON.parse(JSON.stringify(job[0]))[key];
 exports.checkEquality = (tag, query) => tag && query ? tag.toLowerCase() === query.toLowerCase() : false;
 exports.getFirstCharIfNoImage = (path, name) => path ? '' : name.charAt(0); 
+exports.getMatchTitle = function(hash, type) {
+    let str = ''
+    switch (hash.mn) {
+        case '1':
+            str = '1st';
+            break;
+        case '2':
+            str = '2nd';
+            break;
+        case '3':
+            str = '3rd';
+            break;
+        case 'Only':
+            str = 'Only';
+            break;
+        default:
+            str = `${hash.mn}th`;
+            break;
+    }
+    str += ` ${type}, ${hash.t1} v ${hash.t2} in ${hash.vn} at ${getDate(hash.dt)}`
+    return str
+}
+function getDate(d) {
+    return new Date(d)
+}
