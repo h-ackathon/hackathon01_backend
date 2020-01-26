5314e94d6a179c44cfc5b1ffe614d9e7388f6dc1
Bilal Asif <bilal.asif@cricingif.com>
2020-01-26 08:23:23 +0500

    match model

--- model/match.js
+++ model/match.js
@@ -2,11 +2,12 @@ const mongoose = require('mongoose');
 const {Schema} = mongoose;
 
 const MatchSchema = new Schema({
-    title: String,
-    start_date_time: String,
-    end_date_time: String,
-    teams: [{type: String, ref: 'teams'}],
-    league: {type: String, ref: 'leagues'}
+    title: {type: String, required: [true, 'Title is required']},
+    start_date_time: {type: Date, required: [true, 'Start Date is required']},
+    end_date_time: {type: Date, required: [true, 'End Date is required']},
+    type: {type: String, required: [true, 'Type is required']},
+    teams: [{type: Schema.Types.ObjectId, ref: 'teams', required: [true, 'Teams are required']}],
+    league: {type: Schema.Types.ObjectId, ref: 'leagues', required: [true, 'League is required']}
 });
 
 module.exports = mongoose.model('matches', MatchSchema);
